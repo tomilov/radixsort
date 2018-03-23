d7cd2ec75e118847371f5b2df47bf302872ed271
Anatoliy Tomilov <tomilovanatoliy@gmail.com>
2018-03-24 00:53:00 +0500

    #

--- main.cpp
+++ main.cpp
@@ -12,14 +12,19 @@
 
 using size_type = std::uint32_t;
 
-alignas(__m128i) size_type v[1200000];
+namespace
+{
+
+alignas(__m128i) size_type v[10000];
 alignas(__m128i) size_type s[std::size(v)];
 
 constexpr size_type bits = std::numeric_limits< unsigned char >::digits;
 constexpr size_type bytes = sizeof(size_type) / sizeof(unsigned char);
 constexpr size_type mask = (1 << bits) - 1;
 
-alignas(32) size_type bit[bytes][1 << bits] = {0};
+alignas(64) size_type byte[bytes][1 << bits] = {};
+
+}
 
 int main()
 {
@@ -83,7 +88,7 @@ int main()
         auto start = std::chrono::high_resolution_clock::now();
     #if 1
         const size_type m = std::size(v);
-        constexpr size_type l = sizeof(__m128i) / bytes;
+        constexpr size_type l = sizeof(__m128i) / sizeof(size_type);
         const size_type n = (m + (l - 1)) / l;
         asm
         (
@@ -129,7 +134,7 @@ int main()
         "jnz bloop;"
         :
         : [n]"r"(n), [v]"r"(v + 0), [offset]"r"(0),
-          [bit0]"m"(bit[0]), [bit1]"m"(bit[1]), [bit2]"m"(bit[2]), [bit3]"m"(bit[3])
+          [bit0]"m"(byte[0]), [bit1]"m"(byte[1]), [bit2]"m"(byte[2]), [bit3]"m"(byte[3])
         : "memory", "cc",
           "%xmm0"
         );
@@ -145,25 +150,25 @@ int main()
         size_type sum[bytes] = {0, 0, 0, 0};
         for (size_type b = 0; b < bytes; ++b) {
             for (size_type i = 0; i < (1 << bits); ++i) {
-                sum[b] += std::exchange(bit[b][i], sum[b]);
+                sum[b] += std::exchange(byte[b][i], sum[b]);
             }
         }
         std::cout << std::chrono::duration_cast< std::chrono::microseconds >(std::chrono::high_resolution_clock::now() - start).count() << std::endl;
         for (const size_type & e : v) {
             _mm_prefetch(&e + 256, _MM_HINT_T2);
-            s[bit[0][e & mask]++] = e;
+            s[byte[0][e & mask]++] = e;
         }
         for (const size_type & e : s) {
             _mm_prefetch(&e + 256, _MM_HINT_T2);
-            v[bit[1][(e >> (1 * bits)) & mask]++] = e;
+            v[byte[1][(e >> (1 * bits)) & mask]++] = e;
         }
         for (const size_type & e : v) {
             _mm_prefetch(&e + 256, _MM_HINT_T2);
-            s[bit[2][(e >> (2 * bits)) & mask]++] = e;
+            s[byte[2][(e >> (2 * bits)) & mask]++] = e;
         }
         for (const size_type & e : s) {
             _mm_prefetch(&e + 256, _MM_HINT_T2);
-            v[bit[3][e >> (3 * bits)]++] = e;
+            v[byte[3][e >> (3 * bits)]++] = e;
         }
         std::cout << "radix sort " << std::chrono::duration_cast< std::chrono::microseconds >(std::chrono::high_resolution_clock::now() - start).count() << std::endl;
         assert(std::is_sorted(std::cbegin(v), std::cend(v)));
